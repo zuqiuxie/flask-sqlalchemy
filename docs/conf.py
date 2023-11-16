86521566a6a3b5d679d62ee9ae61ac349790672a
彬少 <dz303241@163.com>
2023-11-16 19:02:21 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ issues_github_path = "pallets-eco/flask-sqlalchemy"
 
 # HTML -----------------------------------------------------------------
 
-html_theme = "sphinx"
+html_theme = "insegel"
 html_context = {
     "project_links": [
         ProjectLink("Donate", "https://palletsprojects.com/donate"),
